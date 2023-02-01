9909186e2e89f7f9c5aa52a0b32432ed2d29ae6e
Jason Bongiovanni <jpbon@protonmail.com>
2023-02-01 10:03:04 -0600

    updated to postgresql

--- WDD2/settings.py
+++ WDD2/settings.py
@@ -69,10 +69,21 @@ WSGI_APPLICATION = 'WDD2.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': str(BASE_DIR / 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(BASE_DIR / 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'WDD',
+        'USER': 'postgres',
+        'PASSWORD': config('DBPW'),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
